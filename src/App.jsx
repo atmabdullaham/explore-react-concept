f8458c819dc8c2cc7129df334017e305bcae07f4
atmabdullaham <atmabdullaham@gmail.com>
2025-01-26 09:15:44 +0600

    JSX, its rules and add dynamic content to JSX in your first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Vite + React</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Student></Student>
+    </>
+  );
+}
+function Person() {
+  const age = 25;
+  const money = 20;
+  const person = { name: "Majed", age: 12 };
+  return (
+    <h3>
+      {" "}
+      I am {person.name} with age {age + money}{" "}
+    </h3>
+  );
+}
+
+function Student() {
+  return (
+    <>
+      <h3>This is a student</h3>
+      <p>Name:</p>
+      <p>Age:</p>
     </>
   );
 }
